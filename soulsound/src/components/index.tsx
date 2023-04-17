67b5c077ad72738bfcfe19ad7796edeabc39a3db
unknown <mikeatilano.dev@gmail.com>
2023-04-17 16:55:12 +0800

    Removed FooterBanner

--- soulsound/src/components/index.tsx
+++ soulsound/src/components/index.tsx
@@ -3,6 +3,5 @@ export { default as Layout } from "./Layout";
 export { default as HeroBanner } from "./HeroBanner/HeroBanner";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Product } from "./Product/Product";
-export { default as FooterBanner } from "./FooterBanner.js/FooterBanner";
 export { default as Cart } from "./Cart/Cart";
 export { default as ImageGrid } from "./ImageGrid/ImageGrid";
